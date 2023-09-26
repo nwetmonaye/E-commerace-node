e490dd2bcf2be6e997b2050d21188b01035e467d
nwetmonaye <nwetmon7@gamil.com>
2023-09-26 09:49:50 +0630

    node-learning

--- routes/admin.js
+++ routes/admin.js
@@ -2,7 +2,6 @@ const path = require('path');
 
 const express = require('express');
 
-// const rootDir = require('../util/path');
 const adminController = require('../controller/admin');
 
 const router = express.Router();
@@ -15,5 +14,11 @@ router.get('/products', adminController.getProducts);
 
 // /admin/add-product => POST
 router.post('/add-product', adminController.postAddProduct);
+
+router.get('/edit-product/:productId', adminController.getEditProduct);
+
+router.post('/edit-product', adminController.postEditProduct);
+
+router.post('/delete-product', adminController.postDeleteProduct);
 
 module.exports = router;
--- routes/shop.js
+++ routes/shop.js
@@ -2,8 +2,6 @@ const path = require('path');
 
 const express = require('express');
 
-// const rootDir = require('../util/path');
-// const adminData = require('./admin');
 const shopController = require('../controller/shop');
 
 const router = express.Router();
@@ -12,7 +10,13 @@ router.get('/', shopController.getIndex);
 
 router.get('/products', shopController.getProducts);
 
+router.get('/products/:productId', shopController.getProduct);
+
 router.get('/cart', shopController.getCart);
+
+router.post('/cart', shopController.postCart);
+
+router.post('/cart-delete-item',shopController.postCartDeleteProduct);
 
 router.get('/orders', shopController.getOrders);
 
